514349080afe7a748d4ef8b186eb90644ba2c3a7
Chris <chris.painter@comcast.net>
2018-06-11 20:15:53 +0200

    Added basic register call for server

--- src/pages/models.ts/User.ts
+++ src/pages/models.ts/User.ts
@@ -1,7 +1,7 @@
 import { Charity } from '../models.ts/Charity';
 
 export class User {
-    name: string;
+    username: string;
     password: string;
     charitiesDonatedTo: Array<Charity> = [];
     charitiesNotDonatedTo: Array<Charity> = []
